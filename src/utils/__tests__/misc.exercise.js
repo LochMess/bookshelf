c2f863381bb727a5670f7728a4470b373493adec
LochMess <18762221+LochMess@users.noreply.github.com>
2022-02-23 12:55:56 +1100

    Exercise and extra credit 1

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,11 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  const date = new Date(2020, 2, 1)
+  expect(formatDate(date)).toBe('Mar 20')
+})
+
+test('formatDate formats the date for years before 2000', () => {
+  const date = new Date(1971, 0, 19)
+  expect(formatDate(date)).toBe('Jan 71')
+})
